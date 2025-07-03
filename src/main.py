64f16b08152135e471bf5b6592080c0ecd8ce329
Mohammed Babelly <mohammed@nevercode.io>
2025-07-03 17:43:03 +0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from codepush_mcp import mcp
 
 if __name__ == "__main__":
     mcp.run(
-        transport="stramable-http",
+        transport="http",
         host="0.0.0.0",
         port=4200,
         log_level="debug",
