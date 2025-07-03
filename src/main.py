9c042f1faaf3a75a7d5ca62e844ca8d217ed05b7
Mohammed Babelly <mohammed@nevercode.io>
2025-07-03 17:37:56 +0400

    change transport

--- src/main.py
+++ src/main.py
@@ -2,4 +2,9 @@ from codepush_mcp import mcp
 
 
 if __name__ == "__main__":
-    mcp.run()
+    mcp.run(
+        transport="stramable-http",
+        host="0.0.0.0",
+        port=4200,
+        log_level="debug",
+    )
